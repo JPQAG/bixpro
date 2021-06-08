cb2bbcdaae438ff81a4ebd526cccb30e17f40c2c
James Price <janthonyprice@gmail.com>
2021-06-08 10:46:09 +1000

    Security Data Feed added to table

--- build/static/js/xlsx.55c6b36d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdatta-able-react-hook"]=this["webpackJsonpdatta-able-react-hook"]||[]).push([[22],{1647:function(t,a){},2209:function(t,a){},2210:function(t,a){}}]);
-//# sourceMappingURL=xlsx.55c6b36d.chunk.js.map
