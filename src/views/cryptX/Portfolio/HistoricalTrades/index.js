a02f901c7431a01a2795d7588bf32c638e966d5e
James Price <janthonyprice@gmail.com>
2021-06-11 10:00:24 +1000

    Working on Historical Trades Feed

--- src/views/cryptX/Portfolio/HistoricalTrades/index.js
+++ src/views/cryptX/Portfolio/HistoricalTrades/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import Breadcrumb from '../../../../../layouts/AdminLayout/Breadcrumb';
+import FullTable from '../../../../components/cryptX/tables/React/AllTradesTable.js';
 
-const ComingSoon = () => {
+
+const Markets = () => {
     return (
         <React.Fragment>
-            {/* <Breadcrumb/> */}
-            <div className="auth-wrapper offline">
-                <div className="text-center">
-                    <h1 className="mb-4">Coming Soon</h1>
-                    <h5 className="text-muted mb-4">Our site is under construction - 10 days to go</h5>
-                    <NavLink to="/" className="btn btn-primary mb-4"><i className="feather icon-home"/>Back to Home</NavLink>
-                </div>
-            </div>
+            <FullTable></FullTable>
         </React.Fragment>
     );
 };
 
-export default ComingSoon;
+export default Markets;
