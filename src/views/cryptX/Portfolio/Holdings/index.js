6cb78137468e0053e45b2738222c032c3512a38d
James Price <janthonyprice@gmail.com>
2021-06-15 11:24:44 +1000

    Just recieved 'react-scripts' is not recognised error again

--- src/views/cryptX/Portfolio/Holdings/index.js
+++ src/views/cryptX/Portfolio/Holdings/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {Row, Col, Card} from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import FullTable from '../../../../components/cryptX/tables/React/AllHoldingsTable.js';
-
+import PieDonutChart from "../../../../components/cryptX/charts/chart/PieDonutChart.js";
 
 const Markets = () => {
     return (
         <React.Fragment>
-            <FullTable></FullTable>
+            <Row>
+                <Col xl={12} md={10} >
+                    <Card>
+                        <Card.Header>
+                            <Card.Title as="h5">Holdings Breakdown</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <PieDonutChart></PieDonutChart>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xl={12} md={10} >
+                    <FullTable></FullTable>
+                </Col>
+            </Row>
         </React.Fragment>
     );
 };
