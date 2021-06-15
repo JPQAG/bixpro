a6a3cba14b7992f1031bf0d487dde12c805f9729
James Price <janthonyprice@gmail.com>
2021-06-15 15:22:52 +1000

    Finished Pie Charts for Holdings

--- src/views/cryptX/Portfolio/Holdings/index.js
+++ src/views/cryptX/Portfolio/Holdings/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import {Row, Col, Card} from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import FullTable from '../../../../components/cryptX/tables/React/AllHoldingsTable.js';
-import PieDonutChart from "../../../../components/cryptX/charts/chart/PieDonutChart.js";
+import HoldingsQtyPieDonutChart from "../../../../components/cryptX/charts/chart/HoldingsQtyPieDonutChart.js";
+import HoldingsValPieDonutChart from "../../../../components/cryptX/charts/chart/HoldingsValPieDonutChart.js";
 
 const Markets = () => {
     return (
         <React.Fragment>
             <Row>
-                <Col xl={12} md={10} >
+                <Col xl={6} md={10} >
                     <Card>
                         <Card.Header>
-                            <Card.Title as="h5">Holdings Breakdown</Card.Title>
+                            <Card.Title as="h5">Portfolio Composition by Quantity</Card.Title>
                         </Card.Header>
                         <Card.Body>
-                            <PieDonutChart></PieDonutChart>
+                            <HoldingsQtyPieDonutChart></HoldingsQtyPieDonutChart>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xl={6} md={10} >
+                    <Card>
+                        <Card.Header>
+                            <Card.Title as="h5">Portfolio Composition by Valuation</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <HoldingsValPieDonutChart></HoldingsValPieDonutChart>
                         </Card.Body>
                     </Card>
                 </Col>
