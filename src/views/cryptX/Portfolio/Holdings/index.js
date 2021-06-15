0b39265bca999823c70b9f0a4b3e2e2a802378f9
James Price <janthonyprice@gmail.com>
2021-06-15 16:55:44 +1000

    Finished Formatted Wallet Cards

--- src/views/cryptX/Portfolio/Holdings/index.js
+++ src/views/cryptX/Portfolio/Holdings/index.js
@@ -4,15 +4,30 @@ import { NavLink } from 'react-router-dom';
 import FullTable from '../../../../components/cryptX/tables/React/AllHoldingsTable.js';
 import HoldingsQtyPieDonutChart from "../../../../components/cryptX/charts/chart/HoldingsQtyPieDonutChart.js";
 import HoldingsValPieDonutChart from "../../../../components/cryptX/charts/chart/HoldingsValPieDonutChart.js";
+import BitcoinWalletValCard from "../../../../components/cryptX/cards/BitcoinWalletVal.js"
+import EthereumWalletValCard from "../../../../components/cryptX/cards/EthereumWalletVal.js"
+import TotalWalletValCard from "../../../../components/cryptX/cards/TotalWalletVal.js"
 
 const Markets = () => {
+
+
+
     return (
         <React.Fragment>
             <Row>
+                <Col xl={4} md={6} >
+                    <BitcoinWalletValCard></BitcoinWalletValCard>
+                </Col>
+                <Col md={6} xl={4}>
+                    <EthereumWalletValCard></EthereumWalletValCard>
+                </Col>
+                <Col md={12} xl={4}>
+                    <TotalWalletValCard></TotalWalletValCard>
+                </Col>
                 <Col xl={6} md={10} >
                     <Card>
                         <Card.Header>
-                            <Card.Title as="h5">Portfolio Composition by Quantity</Card.Title>
+                            <Card.Title as="h5">Portfolio Composition</Card.Title>
                         </Card.Header>
                         <Card.Body>
                             <HoldingsQtyPieDonutChart></HoldingsQtyPieDonutChart>
@@ -22,7 +37,7 @@ const Markets = () => {
                 <Col xl={6} md={10} >
                     <Card>
                         <Card.Header>
-                            <Card.Title as="h5">Portfolio Composition by Valuation</Card.Title>
+                            <Card.Title as="h5">Portfolio Composition</Card.Title>
                         </Card.Header>
                         <Card.Body>
                             <HoldingsValPieDonutChart></HoldingsValPieDonutChart>
