51bdd4cec0de33fb69a6143476a053140ca8fb7e
James Price <janthonyprice@gmail.com>
2021-06-16 10:36:04 +1000

    Finished Market Price Change Bar Chart

--- src/views/cryptX/MacroView/Dashboard/Markets/index.js
+++ src/views/cryptX/MacroView/Dashboard/Markets/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import FullTable from '../../../../../components/cryptX/tables/React/AllCoinsTable';
 import MarketsMarketCapPieDonutChart from '../../../../../components/cryptX/charts/chart/MarketsMarketCapPieDonutChart.js';
+import OneDayReturnBarChart from '../../../../../components/cryptX/charts/chart/OneDayReturnBarChart.js';
+
 import {Row, Col, Card} from 'react-bootstrap';
 
 import axios from 'axios';
@@ -10,13 +12,23 @@ const Markets = () => {
     return (
         <React.Fragment>
             <Row>
-                <Col xl={12} md={10} >
+                <Col xl={6} md={10} >
                     <Card>
                         <Card.Header>
                             <Card.Title as="h5">Market Composition</Card.Title>
                         </Card.Header>
                         <Card.Body>
                             <MarketsMarketCapPieDonutChart></MarketsMarketCapPieDonutChart>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xl={6} md={10} >
+                    <Card>
+                        <Card.Header>
+                            <Card.Title as="h5">Market Performance</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <OneDayReturnBarChart></OneDayReturnBarChart>
                         </Card.Body>
                     </Card>
                 </Col>
