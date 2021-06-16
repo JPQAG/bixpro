8fee834e57a30a0f93cd97f8b54bc04f69e4c6f7
James Price <janthonyprice@gmail.com>
2021-06-16 14:47:44 +1000

    Complete Market Bubble Chart

--- src/views/cryptX/MacroView/Dashboard/Markets/index.js
+++ src/views/cryptX/MacroView/Dashboard/Markets/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import FullTable from '../../../../../components/cryptX/tables/React/AllCoinsTable';
 import MarketsMarketCapPieDonutChart from '../../../../../components/cryptX/charts/chart/MarketsMarketCapPieDonutChart.js';
 import OneDayReturnBarChart from '../../../../../components/cryptX/charts/chart/OneDayReturnBarChart.js';
+import MarketsBubbleChart from '../../../../../components/cryptX/charts/chart/MarketsBubbleChart.js';
 
 import {Row, Col, Card} from 'react-bootstrap';
 
@@ -29,6 +30,18 @@ const Markets = () => {
                         </Card.Header>
                         <Card.Body>
                             <OneDayReturnBarChart></OneDayReturnBarChart>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={12} md={20} >
+                    <Card>
+                        <Card.Header>
+                            <Card.Title as="h5">Charting the Market</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <MarketsBubbleChart></MarketsBubbleChart>
                         </Card.Body>
                     </Card>
                 </Col>
